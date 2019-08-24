9fd03a88c86b2bb814fbc504f49a3dd962966750
Russell <russellrok@.com>
2019-08-24 14:17:08 -0700

    First Commit From Command Line

--- ValidatingData.py
+++ ValidatingData.py
@@ -1,14 +1,15 @@
 from osgeo import ogr
 
-propertiesFile = "/home/russell/Documents/sampleData/Canada.shp"
+propertiesFile = "/home/russell/DisasterResponse/validateData/tl_2015_36_prisecroads.shp"
 
-def helloworld():
-   print("helloworld")
 
 '''
 Name: geom_type
 Date 2019-08-20
 Author: Russell Brown
+Purpose: Read the Geometry Types
+
+Vatiable: pathToData
 '''
 def geom_type(pathToData):
     shapefile = ogr.Open(pathToData)
@@ -16,14 +17,61 @@ def geom_type(pathToData):
     List = []
     for feature in layer:
         geometry = feature.GetGeometryRef()
-        print(geometry.GetGeometryName())
-        List.append(geometry.GetGeometryName)
+        #print(geometry.GetGeometryName())
+        List.append(geometry.GetGeometryName())
+   
+    
     return List
 
-def ValidateList():
+'''
+Name: ValidateList
+Date: 2019-08-24
+Author: Russell Brown
+Purpose check to make sure all data are equal to LineString, 
+If all are equal to a LineString return true,
+If not all LineString return False
 
 
+Variables:
+	geometryTypes: Is a List of all the different Geometry types that were in the shapefile being looked at.
+
+'''
+def ValidateList(geometryTypes):
+	results = False
+	if(geometryTypes.count(geometryTypes[0]) == len(geometryTypes) and geometryTypes[0]=="LINESTRING"):
+		results = True	
+	return results	
+
+'''
+Name: GetAll Points
+Date: 2019-08-24
+Author: Russell Brown
+Purpose: To open up the shapefile and read all the points and store them into an array.
+
+Variables: 
+	pathToDate: used to read in all the data points and return them to the main method
+	
+
+'''
+def GetAllPoints(pathToData):
+	file = ogr.Open("my_shapefile.shp")
+	shape = file.GetLayer(0)
+	#sfirst feature of the shapefile
+	feature = shape.GetFeature(0)
+	first = feature.ExportToJson()
+	print first
+
+
+
+'''
+Name:Main Method
+Date:2019-08-24
+Author:Russell Brown
+Purpose: The main method used to call all the other methods.
+'''
 if __name__ == '__main__':
-    helloworld()
-    dataTypes = geom_type(propertiesFile)
-    ValidateList()
+	dataTypes = geom_type(propertiesFile)
+	results = ValidateList(dataTypes)
+	GetAllPoints(propertiesFile)
+	print(results)
+	
